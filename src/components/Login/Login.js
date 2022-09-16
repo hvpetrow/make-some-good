822e260ed09599c96219d2a6da01230c7cc8e55b
= <hvpetrow@gmail.com>
2022-09-16 16:33:07 +0200

    forgot-password link added

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -121,8 +121,16 @@ export const Login = () => {
                                         Log In
                                     </button>
                                 </div>
-                                <p className=" text-gray-500 dark:text-gray-400">
-                                    Need an account?{" "}
+                                <div className="flex justify-center -mt-1 mb-7">
+                                    <Link
+                                        to="/forgot-password"
+                                        className="text-blue-600 hover:text-blue-800 focus:text-blue-700 active:text-blue-800 duration-200 transition ease-in-out"
+                                    >
+                                        Forgot Password?
+                                    </Link>
+                                </div>
+                                <p className="flex justify-center text-gray-500 dark:text-gray-400">
+                                    Need an account?&nbsp;
                                     <Link
                                         to="/register"
                                         className="text-blue-600 hover:text-blue-800 focus:text-blue-700 active:text-blue-800 duration-200 transition ease-in-out"
