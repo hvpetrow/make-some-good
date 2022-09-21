f96b9419bee381ac36c2ff97960551d5e4f7b948
= <hvpetrow@gmail.com>
2022-09-21 19:21:35 +0200

    CardTemplate added

--- src/components/Home/CardTemplate.js
+++ src/components/Home/CardTemplate.js
@@ -1,14 +1,16 @@
+import { Link } from "react-router-dom";
+
 export const CardTemplate = ({cause}) => {
     console.log(cause);
     return (
-        <div className="rounded-lg shadow-lg bg-white max-w-md transition duration-500 hover:scale-105 hover:bg-gray-300">
-            <a href="#!">
+        <div className="rounded-lg shadow-lg bg-white max-w-sm transition duration-500 hover:scale-105 hover:bg-gray-300">
+            <Link to={`/details/${cause.id}`}>
                 <img
                     className="rounded-t-lg"
                     src={cause.imgUrl}
                     alt="causeImg"
                 />
-            </a>
+            </Link>
             <div className="p-6">
                 <h5 className="text-gray-900 text-xl font-medium mb-2">{cause.title}</h5>
                 <p className="text-gray-700 text-base mb-4">
@@ -16,7 +18,7 @@ export const CardTemplate = ({cause}) => {
                 </p>
                 <button
                     type="button"
-                    className=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
+                    className="inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
                 >
                     Details
                 </button>
