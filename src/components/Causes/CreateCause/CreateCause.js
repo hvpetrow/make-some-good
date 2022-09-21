da0bb5dda0023e9a4dcfbfafb11812d25584de7c
= <hvpetrow@gmail.com>
2022-09-21 17:28:06 +0200

    updated

--- src/components/Causes/CreateCause/CreateCause.js
+++ src/components/Causes/CreateCause/CreateCause.js
@@ -28,7 +28,6 @@ export const CreateCause = () => {
     const [error, setError] = useState('');
     const [isLoading, setIsLoading] = useState(false);
 
-
     const changeHandler = (e) => {
         setValues((oldValues) => ({
             ...oldValues,
@@ -156,6 +155,7 @@ export const CreateCause = () => {
                                 className="inline-block px-7 py-3 bg-blue-600 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out w-full"
                                 data-mdb-ripple="true"
                                 data-mdb-ripple-color="light"
+                                disabled={isLoading}
                             >
                                 Create
                             </button>
