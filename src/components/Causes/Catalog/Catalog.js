b9983852cf4c5c807ae5fd5c45a130588d21b924
= <hvpetrow@gmail.com>
2022-09-22 16:31:56 +0200

    spinner added

--- src/components/Causes/Catalog/Catalog.js
+++ src/components/Causes/Catalog/Catalog.js
@@ -1,10 +1,11 @@
 import { collection } from "firebase/firestore";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 import { useAuth } from "../../../contexts/AuthContext";
 import { useCausesContext } from "../../../contexts/CauseContext";
 import { db } from "../../../firebase";
 import { getAll } from "../../../services/crudService";
+import { Spinner } from "../../../shared/Spinner";
 import { CardTemplate } from "./CardTemplate";
 
 
@@ -14,6 +15,7 @@ const causesCollectionRef = collection(db, "causes");
 
 export const Catalog = () => {
     const { currentUser } = useAuth();
+    const [isLoading, setIsLoading] = useState(true);
     const { causes,setCauses } = useCausesContext();
 
     if (currentUser) { 
@@ -37,6 +39,8 @@ export const Catalog = () => {
                     });
                     
                     setCauses(arr)
+                }).then(() => {
+                    setIsLoading(false);
                 });
         } catch (error) {
             console.log(error);
@@ -48,10 +52,12 @@ export const Catalog = () => {
     return (
         <div className="flex justify-center my-7">
             <div className="grid py-10 justify-center my-7 -space-x-15 grid-cols-1  sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-14">
-            {causes.length
-                ? causes.map(c => <CardTemplate key={c.id} id={c.id} cause={c.fields} />)
-                : <h3 className="no-articles">No articles yet</h3>
-            }
+            {isLoading
+                        ? (<Spinner />)
+                        : causes.length !== 0
+                            ? (causes.map(c => <CardTemplate key={c.id} id={c.id} cause={c.fields} />))
+                            : (<h3 className="no-articles">No articles yet</h3>)
+                    }
             </div>
         </div>
     );
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,11 @@
 import { collection } from "firebase/firestore";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 import { useAuth } from "../../contexts/AuthContext";
 import { useCausesContext } from "../../contexts/CauseContext";
 import { db } from "../../firebase";
 import { getAll } from "../../services/crudService";
+import { Spinner } from "../../shared/Spinner";
 import { CardTemplate } from "./CardTemplate";
 
 const causesCollectionRef = collection(db, "causes");
@@ -13,6 +14,8 @@ const causesCollectionRef = collection(db, "causes");
 export const Home = () => {
     const { currentUser } = useAuth();
     const { causes, setCauses } = useCausesContext();
+    const [isLoading, setIsLoading] = useState(true);
+
 
     if (currentUser) {
         console.log(currentUser.uid);
@@ -35,7 +38,10 @@ export const Home = () => {
                     });
 
                     setCauses(arr)
-                });
+            
+            }).then(() => {
+                setIsLoading(false);
+            });
         } catch (error) {
             console.log(error);
         }
@@ -48,9 +54,11 @@ export const Home = () => {
             <h1 className="flex justify-center text-center my-7">Home Page</h1>
             <div className="flex justify-center my-7">
                 <div className="grid py-10 justify-center my-7 -space-x-15 grid-cols-1  sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-14">
-                    {causes.length
-                        ? causes.map(c => <CardTemplate key={c.id} id={c.id} cause={c.fields} />)
-                        : <h3 className="no-articles">No articles yet</h3>
+                {isLoading
+                        ? (<Spinner />)
+                        : causes.length !== 0
+                            ? (causes.map(c => <CardTemplate key={c.id} id={c.id} cause={c.fields} />))
+                            : (<h3 className="no-articles">No articles yet</h3>)
                     }
                 </div>
             </div>
