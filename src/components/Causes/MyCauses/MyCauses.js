8104321a334fb0d48be76ec6716b6bb82ec4b184
= <hvpetrow@gmail.com>
2022-09-22 15:27:09 +0200

    myCauses component added

--- src/components/Causes/MyCauses/MyCauses.js
+++ src/components/Causes/MyCauses/MyCauses.js
@@ -6,6 +6,7 @@ import { useCausesContext } from "../../../contexts/CauseContext";
 import { getAll } from "../../../services/crudService";
 import { db } from "../../../firebase";
 import { CardTemplate } from "../Catalog/CardTemplate";
+import { useState } from "react";
 
 const causesCollectionRef = collection(db, "causes");
 
@@ -13,6 +14,7 @@ const causesCollectionRef = collection(db, "causes");
 export const MyCauses = () => {
     const { currentUser } = useAuth();
     const { causes, setCauses } = useCausesContext();
+    const [myCauses, setMyCauses] = useState([]);
 
     if (currentUser) {
         console.log(currentUser.uid);
@@ -34,23 +36,32 @@ export const MyCauses = () => {
 
                     });
 
-                    setCauses(arr)
+                    setCauses(arr);
+
+                    if (arr.length > 0) {
+                        const myCausesArr = arr.filter(c => c.fields.creator === currentUser.uid);
+                        setMyCauses(myCausesArr);
+                    }
+
                 });
         } catch (error) {
             console.log(error);
         }
     }, []);
 
+
+
     console.log(causes);
+    console.log(myCauses);
 
     return (
         <>
             <h1 className="flex justify-center text-center my-7">My causes Page</h1>
             <div className="flex justify-center my-7">
                 <div className="grid py-10 justify-center my-7 -space-x-15 grid-cols-1  sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-14">
-                    {causes.length
+                    {myCauses.length
                         ? causes.map(c => <CardTemplate key={c.id} id={c.id} cause={c.fields} />)
-                        : <h3 className="no-articles">No articles yet</h3>
+                        : <h3 className="no-articles">You do not have added causes yet</h3>
                     }
                 </div>
             </div>
