021d947b08c568ea248b032f331d95e8f4c802f1
= <hvpetrow@gmail.com>
2022-09-24 17:08:25 +0200

    updated

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -17,19 +17,21 @@ export const Home = () => {
     const { currentUser } = useAuth();
     const { causes, setCauses } = useCausesContext();
     const [isLoading, setIsLoading] = useState(true);
-    const [latestDoc,setLatestDoc] = useState(0);
-    const [clickable,setClickable] = useState(true);
-    const [visible,setVisible] = useState(true);
+    const [latestDoc, setLatestDoc] = useState(0);
+    const [clickable, setClickable] = useState(true);
+    const [visible, setVisible] = useState(true);
 
 
     const causesCollectionRef = collection(db, "causes");
-    const orderedQuery = query(causesCollectionRef,orderBy("title"),startAfter(latestDoc || 0),limit(3));
+    const orderedQuery = query(causesCollectionRef, orderBy("title"), startAfter(latestDoc || 0), limit(3));
 
     if (currentUser) {
         console.log(currentUser.uid);
     }
 
     useEffect(() => {
+        // const debounceHandleScroll = debounce(loadMoreScrollHandler, 1000); // for infinite scroll
+        // window.addEventListener('scroll', debounceHandleScroll);
         try {
             getAll(orderedQuery)
                 .then(docs => {
@@ -45,13 +47,12 @@ export const Home = () => {
                         console.log(doc.id, " => ", doc.data());
                     });
 
-
                     setCauses(arr);
-                    setLatestDoc(docs.docs[docs.docs.length-1]);
-                    console.log("LATEST DOC" , latestDoc);
-            }).then(() => {
-                setIsLoading(false);
-            });
+                    setLatestDoc(docs.docs[docs.docs.length - 1]);
+                    console.log("LATEST DOC", latestDoc);
+                }).then(() => {
+                    setIsLoading(false);
+                });
         } catch (error) {
             console.log(error);
         }
@@ -59,11 +60,12 @@ export const Home = () => {
 
     const loadMoreClickHandler = async (e) => {
         console.log("load more clicked");
+
         try {
             getAll(orderedQuery)
                 .then(docs => {
                     if (docs.empty) {
-                       setClickable(false);
+                        setClickable(false);
                         return;
                     }
                     let arr = [];
@@ -78,32 +80,32 @@ export const Home = () => {
                         console.log(doc.id, " => ", doc.data());
                     });
 
-                    
+
                     setCauses(oldArr => [
                         ...oldArr,
                         ...arr
                     ]);
 
 
-                    console.log("LATEST DOC" , latestDoc);
-
-                    setLatestDoc(docs.docs[docs.docs.length-1]);
-            }).then(() => {
-                setIsLoading(false);
-            });
+                    console.log("LATEST DOC", latestDoc);
+                    setLatestDoc(docs.docs[docs.docs.length - 1]);
+                }).then(() => {
+                    setIsLoading(false);
+                });
         } catch (error) {
             console.log(error);
         }
     }
+
 
     console.log(causes);
 
     return (
         <>
             <h1 className="flex justify-center text-center my-7">Home Page</h1>
-            <div className="flex justify-center my-7">
-                <div className="grid py-10 justify-center my-7 -space-x-15 grid-cols-1  sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-14">
-                {isLoading
+            <div  className=" flex justify-center my-7 ">
+                <div  className="grid py-10 justify-center my-7  -space-x-15 grid-cols-1  sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-14">
+                    {isLoading
                         ? (<Spinner />)
                         : causes.length !== 0
                             ? (causes.map(c => <CardTemplate key={c.id} id={c.id} cause={c.fields} />))
@@ -112,12 +114,15 @@ export const Home = () => {
                 </div>
             </div>
             {visible &&
-            <button id="load-more-button" onClick={clickable ? loadMoreClickHandler : () => {
-                    toast.warning('No more causes');
+                <button id="load-more-button" onClick={clickable ? loadMoreClickHandler : () => {
+                    toast.warning('No more causes', {
+                        position: toast.POSITION.BOTTOM_CENTER
+                      });
                     setVisible(false);
-                    }}>load more</button>
-                }
-            <BackToTheTopButton />
+                }}>load more</button>
+            }
+            {/* <BackToTheTopButton /> */}
+            
         </>
     );
 }
