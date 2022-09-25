0a9e959fc57db5c2ba5872f1fd75b807e0ce471a
= <hvpetrow@gmail.com>
2022-09-25 16:37:08 +0200

    conditional rendering implemented

--- src/components/Causes/Details/Details.js
+++ src/components/Causes/Details/Details.js
@@ -3,6 +3,7 @@ import React from 'react'
 import { useState } from 'react';
 import { useEffect } from 'react'
 import { useParams } from 'react-router-dom';
+import { useAuth } from '../../../contexts/AuthContext';
 import { db } from '../../../firebase';
 import { getOneCause } from '../../../services/causesService'
 import { getOne } from '../../../services/crudService';
@@ -12,6 +13,7 @@ const usersCollectionRef = collection(db, 'users');
 export const Details = () => {
     const [cause, setCause] = useState('');
     const [creator, setCreator] = useState('');
+    const { currentUser } = useAuth();
     const { causeId } = useParams();
     const [isLoading, setIsLoading] = useState(true);
 
@@ -22,25 +24,25 @@ export const Details = () => {
             getOneCause(causeId)
                 .then(doc => {
                     setCause(doc.data());
-                    
+
                 });
 
-                if (cause.creator) {            
-                    getOne(usersCollectionRef,cause.creator)
+            if (cause.creator) {
+                getOne(usersCollectionRef, cause.creator)
                     .then(doc => {
                         console.log(doc);
                         console.log(doc.data());
-                        
+
                         setCreator(doc.data());
                         console.log(creator);
                     });
-                }
+            }
 
-                setIsLoading(false);
-            } catch (error) {
+            setIsLoading(false);
+        } catch (error) {
             console.log(error);
         }
-    }, [causeId,cause.creator])
+    }, [causeId, cause.creator])
 
 
 
@@ -135,55 +137,62 @@ export const Details = () => {
                                 </p>
                             </div>
                         </div>
-                        <div className="flex flex-col w-full relative bottom-0">
-                            <div className="grid grid-cols-3 border-t divide-x text-[#64748b] bg-gray-50 dark:bg-transparent py-3">
-                                <a className="text-[#079c9a] cursor-pointer uppercase text-xs flex flex-row items-center justify-center font-semibold">
-                                    <div className="mr-2 ">
-                                        <svg
-                                            xmlns="http://www.w3.org/2000/svg"
-                                            height="20px"
-                                            viewBox="0 0 24 24"
-                                            width="20px"
-                                            fill="#079c9a"
-                                        >
-                                            <path d="M0 0h24v24H0z" fill="none" />
-                                            <path d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34c-.39-.39-1.02-.39-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z" />
-                                        </svg>
-                                    </div>
-                                    Update
-                                </a>
-                                <a className="text-[#D2042D] cursor-pointer uppercase text-xs flex flex-row items-center justify-center font-semibold">
-                                    <div className="mr-2">
-                                        <svg
-                                            xmlns="http://www.w3.org/2000/svg"
-                                            height="20px"
-                                            viewBox="0 0 24 24"
-                                            width="20px"
-                                            fill="#D2042D"
-                                        >
-                                            <path d="M0 0h24v24H0V0z" fill="none" />
-                                            <path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM8 9h8v10H8V9zm7.5-5l-1-1h-5l-1 1H5v2h14V4z" />
-                                        </svg>
-                                    </div>
-                                    Remove
-                                </a>
-                                <a className="text-[#9c428c] cursor-pointer uppercase text-xs flex flex-row items-center justify-center font-semibold">
-                                    <div className="mr-2">
-                                        <svg
-                                            xmlns="http://www.w3.org/2000/svg"
-                                            height="20px"
-                                            viewBox="0 0 24 24"
-                                            width="20px"
-                                            fill="#9c428c"
-                                        >
-                                            <path d="M0 0h24v24H0z" fill="none" />
-                                            <path d="M19 19H5V5h7V3H5c-1.11 0-2 .9-2 2v14c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2v-7h-2v7zM14 3v2h3.59l-9.83 9.83 1.41 1.41L19 6.41V10h2V3h-7z" />
-                                        </svg>
-                                    </div>
-                                    Join
-                                </a>
+                        {currentUser.uid &&
+                            <div className="flex flex-col w-full relative bottom-0">
+                                <div className="grid grid-cols-3 border-t divide-x text-[#64748b] bg-gray-50 dark:bg-transparent py-3">
+                                    {currentUser.uid === cause.creator &&
+                                        <>
+                                            <a className="text-[#079c9a] cursor-pointer uppercase text-xs flex flex-row items-center justify-center font-semibold">
+                                                <div className="mr-2 ">
+                                                    <svg
+                                                        xmlns="http://www.w3.org/2000/svg"
+                                                        height="20px"
+                                                        viewBox="0 0 24 24"
+                                                        width="20px"
+                                                        fill="#079c9a"
+                                                    >
+                                                        <path d="M0 0h24v24H0z" fill="none" />
+                                                        <path d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34c-.39-.39-1.02-.39-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z" />
+                                                    </svg>
+                                                </div>
+                                                Update
+                                            </a>
+                                            <a className="text-[#D2042D] cursor-pointer uppercase text-xs flex flex-row items-center justify-center font-semibold">
+                                                <div className="mr-2">
+                                                    <svg
+                                                        xmlns="http://www.w3.org/2000/svg"
+                                                        height="20px"
+                                                        viewBox="0 0 24 24"
+                                                        width="20px"
+                                                        fill="#D2042D"
+                                                    >
+                                                        <path d="M0 0h24v24H0V0z" fill="none" />
+                                                        <path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM8 9h8v10H8V9zm7.5-5l-1-1h-5l-1 1H5v2h14V4z" />
+                                                    </svg>
+                                                </div>
+                                                Remove
+                                            </a>
+                                            </>
+                                    }
+                                            <a className="text-[#9c428c] cursor-pointer uppercase text-xs flex flex-row items-center justify-center font-semibold">
+                                                <div className="mr-2">
+                                                    <svg
+                                                        xmlns="http://www.w3.org/2000/svg"
+                                                        height="20px"
+                                                        viewBox="0 0 24 24"
+                                                        width="20px"
+                                                        fill="#9c428c"
+                                                    >
+                                                        <path d="M0 0h24v24H0z" fill="none" />
+                                                        <path d="M19 19H5V5h7V3H5c-1.11 0-2 .9-2 2v14c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2v-7h-2v7zM14 3v2h3.59l-9.83 9.83 1.41 1.41L19 6.41V10h2V3h-7z" />
+                                                    </svg>
+                                                </div>
+                                                Join
+                                            </a>
+                                        
                             </div>
-                        </div>
+                            </div>
+                        }
                     </div>
                 </div>
             </div>
