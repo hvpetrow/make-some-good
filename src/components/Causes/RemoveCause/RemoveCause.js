4bf4daa644f90630bdc3c93ef35546917678f959
= <hvpetrow@gmail.com>
2022-09-26 18:24:13 +0200

    Remove Cause Implemented

---
+++ src/components/Causes/RemoveCause/RemoveCause.js
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import { useNavigate, useParams } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import { deleteCause } from '../../../services/causesService';
+
+export const RemoveCause = () => {
+    const [error, setError] = useState('');
+    const navigate = useNavigate();
+    const { causeId } = useParams();
+
+
+    deleteCause(causeId)
+        .then(() => {
+            navigate('/my-cause', { replace: true });
+            toast.success('Successfully removed cause!');
+        })
+        .catch(() => {
+            setError('Failed deleting');
+            navigate('/', { replace: true });
+        });
+
+    return null;
+}
