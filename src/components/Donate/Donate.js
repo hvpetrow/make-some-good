9bbb4d5586feeebf5262bc6a7f8be50a5a972002
= <hvpetrow@gmail.com>
2022-09-27 19:26:50 +0200

    donate component updated

--- src/components/Donate/Donate.js
+++ src/components/Donate/Donate.js
@@ -1,6 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export const Donate = () => {
+    const [clicked,setClicked] = useState(false);
+
+    const bankAccount = (
+        <div className="bg-gray-300 rounded-lg">
+            <div className="py-4 px-4">
+                <div className="flex flex-col">
+                    <h4 className="text-lg font-semibold mb-3">
+                        Bank Account
+                    </h4>
+                    <div className="flex flex-col text-sm text-gray-700">
+                        <span className="mb-1">
+                           <strong>Bank: </strong>bankName</span>
+                        <span className="mb-1"><strong>IBAN: </strong>111111111</span>
+                        <span className="mb-1"><strong>BIC: </strong>11111111</span>
+                        <span className="mb-1"><strong>Reciever: </strong>recieverName</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+
     return (
         <>
             <div className="h-screen w-full flex flex-col justify-center items-center">
@@ -15,13 +36,13 @@ export const Donate = () => {
                                 amazing things. We're a small team.
                             </p>
                         </div>
-                        <div className="bg-gray-100 rounded-lg">
+                        <div className="bg-gray-200 rounded-lg">
                             <div className="py-4 px-4">
                                 <div className="flex flex-col">
                                     <h4 className="text-lg font-semibold mb-3">
                                         We're currently working on
                                     </h4>
-                                    <div className="flex flex-col text-sm text-gray-500">
+                                    <div className="flex flex-col text-sm text-gray-600">
                                         <span className="mb-1">The next cryptocurrency</span>
                                         <span className="mb-1">Amazing software!</span>
                                         <span className="mb-1">Blockchain technology</span>
@@ -29,13 +50,18 @@ export const Donate = () => {
                                 </div>
                             </div>
                         </div>
+                        {clicked && bankAccount}
                         <div className="py-4">
-                            <a
-                                href="/"
-                                className="block tracking-widest uppercase text-center shadow bg-indigo-600 hover:bg-indigo-700 focus:shadow-outline focus:outline-none text-white text-xs py-3 px-10 rounded"
+                            <button onClick={() => {
+                                setClicked(click => !click)
+                                
+                            }}
+                                className="block w-full tracking-widest uppercase text-center shadow bg-indigo-600 hover:bg-indigo-700 focus:shadow-outline focus:outline-none text-white text-xs py-3 px-10 rounded"
                             >
-                                Go donate
-                            </a>
+                                {clicked 
+                                ? 'Hide'
+                                : 'I want to donate' }
+                            </button>
                         </div>
                     </div>
                 </div>
