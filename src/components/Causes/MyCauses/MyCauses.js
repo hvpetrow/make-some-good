1b0952bf464b9d6d63f6bb0bb5daed8e736352bd
= <hvpetrow@gmail.com>
2022-10-03 19:15:56 +0200

    conditional rendering updated

--- src/components/Causes/MyCauses/MyCauses.js
+++ src/components/Causes/MyCauses/MyCauses.js
@@ -52,7 +52,7 @@ export const MyCauses = () => {
                     }
                 </div>
             </div>
-            {visible &&
+            {visible && myCauses.length !== 0 &&
                 <div className="flex justify-center m-y-5">
                     <button id="load-more-button" className="inline-block px-7 py-3 max-w-sm bg-blue-600 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out w-full"
                         data-mdb-ripple="true"
