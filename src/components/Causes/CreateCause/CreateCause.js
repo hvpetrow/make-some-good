1a6337b873b0deb02b834b778b573b744f6add3b
= <hvpetrow@gmail.com>
2022-12-23 18:36:19 +0100

    styles updated

--- src/components/Causes/CreateCause/CreateCause.js
+++ src/components/Causes/CreateCause/CreateCause.js
@@ -153,18 +153,17 @@ export const CreateCause = () => {
                     </div>
 
                     {/* Submit button */}
-                    <button
-
-                        type="submit"
-                        className="inline-block px-7 py-3 bg-blue-600 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out w-full"
-                        data-mdb-ripple="true"
-                        data-mdb-ripple-color="light"
-                        disabled={isLoading}
-                    >
-                        Create
-                    </button>
-
-
+                    <div className={styles['btn-container']}>
+                        <button
+                            type="submit"
+                            className={styles['submit-btn']}
+                            data-mdb-ripple="true"
+                            data-mdb-ripple-color="light"
+                            disabled={isLoading}
+                        >
+                            Submit
+                        </button>
+                    </div>
                 </form>
             </div>
         </section>
