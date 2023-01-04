59efe4d0e9a79939f7f944c29c3e88a60955c2e7
= <hvpetrow@gmail.com>
2023-01-04 18:19:57 +0100

    styles updated

--- src/components/Causes/Details/Details.js
+++ src/components/Causes/Details/Details.js
@@ -192,21 +192,6 @@ export const Details = () => {
                         <div className={styles['btns-ctn']}>
                             {currentUser.uid === cause.creator &&
                                 <>
-                                    <Link to={`/edit/${causeId}`} className={`${styles['update-btn']} ${styles['btn']}`}>
-                                        <div className="mr-2 ">
-                                            <svg
-                                                xmlns="http://www.w3.org/2000/svg"
-                                                height="20px"
-                                                viewBox="0 0 24 24"
-                                                width="20px"
-                                                fill="#079c9a"
-                                            >
-                                                <path d="M0 0h24v24H0z" fill="none" />
-                                                <path d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34c-.39-.39-1.02-.39-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z" />
-                                            </svg>
-                                        </div>
-                                        Update
-                                    </ Link>
                                     <button onClick={removeHandler} className={`${styles['remove-btn']} ${styles['btn']}`} >
                                         <div className="mr-2">
                                             <svg
@@ -222,10 +207,26 @@ export const Details = () => {
                                         </div>
                                         Remove
                                     </button>
+                                    <Link to={`/edit/${causeId}`} className={`${styles['update-btn']} ${styles['btn']}`}>
+                                        <div className="mr-2 ">
+                                            <svg
+                                                xmlns="http://www.w3.org/2000/svg"
+                                                height="20px"
+                                                viewBox="0 0 24 24"
+                                                width="20px"
+                                                fill="#079c9a"
+                                            >
+                                                <path d="M0 0h24v24H0z" fill="none" />
+                                                <path d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34c-.39-.39-1.02-.39-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z" />
+                                            </svg>
+                                        </div>
+                                        Update
+                                    </ Link>
                                 </>
                             }
                             {!isParticipant
-                                ? <button onClick={joinHandler} className={`${styles['join-btn']} ${styles['btn']}`}>
+                                ? currentUser.uid !== cause.creator &&
+                                (< button onClick={joinHandler} className={`${styles['join-btn']} ${styles['btn']}`}>
                                     <div className="mr-2">
                                         <svg
                                             xmlns="http://www.w3.org/2000/svg"
@@ -239,7 +240,7 @@ export const Details = () => {
                                         </svg>
                                     </div>
                                     Join
-                                </button>
+                                </button>)
                                 : <button onClick={cancelCauseHandler} className={`${styles['cancel-btn']} ${styles['btn']}`}>
                                     <div className="mr-2">
                                         <svg
@@ -261,6 +262,6 @@ export const Details = () => {
                 </div>
             </div>
             }
-        </section>
+        </section >
     )
 }
