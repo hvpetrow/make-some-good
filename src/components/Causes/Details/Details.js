84663a3193756aa5f584e8ded09292651f1e905f
= <hvpetrow@gmail.com>
2023-01-04 16:32:59 +0100

    styles updated

--- src/components/Causes/Details/Details.js
+++ src/components/Causes/Details/Details.js
@@ -189,83 +189,78 @@ export const Details = () => {
                         {cause.description}
                     </p>
                     {currentUser?.uid &&
-                        <div className="flex flex-col w-full relative bottom-0">
-                            <div className="grid grid-cols-3 border-t divide-x text-[#64748b] bg-gray-50 dark:bg-transparent py-3">
-                                {currentUser.uid === cause.creator &&
-                                    <>
-                                        <Link to={`/edit/${causeId}`} className="text-[#079c9a] cursor-pointer uppercase text-xs flex flex-row items-center justify-center font-semibold">
-                                            <div className="mr-2 ">
-                                                <svg
-                                                    xmlns="http://www.w3.org/2000/svg"
-                                                    height="20px"
-                                                    viewBox="0 0 24 24"
-                                                    width="20px"
-                                                    fill="#079c9a"
-                                                >
-                                                    <path d="M0 0h24v24H0z" fill="none" />
-                                                    <path d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34c-.39-.39-1.02-.39-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z" />
-                                                </svg>
-                                            </div>
-                                            Update
-                                        </ Link>
-                                        <button onClick={removeHandler} className="text-[#D2042D] cursor-pointer uppercase text-xs flex flex-row items-center justify-center font-semibold">
-                                            <div className="mr-2">
-                                                <svg
-                                                    xmlns="http://www.w3.org/2000/svg"
-                                                    height="20px"
-                                                    viewBox="0 0 24 24"
-                                                    width="20px"
-                                                    fill="#D2042D"
-                                                >
-                                                    <path d="M0 0h24v24H0V0z" fill="none" />
-                                                    <path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM8 9h8v10H8V9zm7.5-5l-1-1h-5l-1 1H5v2h14V4z" />
-                                                </svg>
-                                            </div>
-                                            Remove
-                                        </button>
-                                    </>
-                                }
-                                {!isParticipant
-                                    ? <button onClick={joinHandler} className="text-[#9c428c] cursor-pointer uppercase text-xs flex flex-row items-center justify-center font-semibold">
+                        <div className={styles['btns-ctn']}>
+                            {currentUser.uid === cause.creator &&
+                                <>
+                                    <Link to={`/edit/${causeId}`} className={`${styles['update-btn']} ${styles['btn']}`}>
+                                        <div className="mr-2 ">
+                                            <svg
+                                                xmlns="http://www.w3.org/2000/svg"
+                                                height="20px"
+                                                viewBox="0 0 24 24"
+                                                width="20px"
+                                                fill="#079c9a"
+                                            >
+                                                <path d="M0 0h24v24H0z" fill="none" />
+                                                <path d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34c-.39-.39-1.02-.39-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z" />
+                                            </svg>
+                                        </div>
+                                        Update
+                                    </ Link>
+                                    <button onClick={removeHandler} className={`${styles['remove-btn']} ${styles['btn']}`} >
                                         <div className="mr-2">
                                             <svg
                                                 xmlns="http://www.w3.org/2000/svg"
                                                 height="20px"
                                                 viewBox="0 0 24 24"
                                                 width="20px"
-                                                fill="#9c428c"
+                                                fill="#D2042D"
                                             >
-                                                <path d="M0 0h24v24H0z" fill="none" />
-                                                <path d="M19 19H5V5h7V3H5c-1.11 0-2 .9-2 2v14c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2v-7h-2v7zM14 3v2h3.59l-9.83 9.83 1.41 1.41L19 6.41V10h2V3h-7z" />
+                                                <path d="M0 0h24v24H0V0z" fill="none" />
+                                                <path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM8 9h8v10H8V9zm7.5-5l-1-1h-5l-1 1H5v2h14V4z" />
                                             </svg>
                                         </div>
-                                        Join
+                                        Remove
                                     </button>
-                                    : <button onClick={cancelCauseHandler} className="text-[#ada02b] cursor-pointer uppercase text-xs flex flex-row items-center justify-center font-semibold">
-                                        <div className="mr-2">
-                                            <svg
-                                                xmlns="http://www.w3.org/2000/svg"
-                                                height="20px"
-                                                viewBox="0 0 24 24"
-                                                width="20px"
-                                                fill="#ada02b"
-                                            >
-                                                <path d="M0 0h24v24H0z" fill="none" />
-                                                <path d="M19 19H5V5h7V3H5c-1.11 0-2 .9-2 2v14c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2v-7h-2v7zM14 3v2h3.59l-9.83 9.83 1.41 1.41L19 6.41V10h2V3h-7z" />
-                                            </svg>
-                                        </div>
-                                        Cancel Cause
-                                    </button>
-                                }
-
-                            </div>
+                                </>
+                            }
+                            {!isParticipant
+                                ? <button onClick={joinHandler} className={`${styles['join-btn']} ${styles['btn']}`}>
+                                    <div className="mr-2">
+                                        <svg
+                                            xmlns="http://www.w3.org/2000/svg"
+                                            height="20px"
+                                            viewBox="0 0 24 24"
+                                            width="20px"
+                                            fill="#9c428c"
+                                        >
+                                            <path d="M0 0h24v24H0z" fill="none" />
+                                            <path d="M19 19H5V5h7V3H5c-1.11 0-2 .9-2 2v14c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2v-7h-2v7zM14 3v2h3.59l-9.83 9.83 1.41 1.41L19 6.41V10h2V3h-7z" />
+                                        </svg>
+                                    </div>
+                                    Join
+                                </button>
+                                : <button onClick={cancelCauseHandler} className={`${styles['cancel-btn']} ${styles['btn']}`}>
+                                    <div className="mr-2">
+                                        <svg
+                                            xmlns="http://www.w3.org/2000/svg"
+                                            height="20px"
+                                            viewBox="0 0 24 24"
+                                            width="20px"
+                                            fill="#ada02b"
+                                        >
+                                            <path d="M0 0h24v24H0z" fill="none" />
+                                            <path d="M19 19H5V5h7V3H5c-1.11 0-2 .9-2 2v14c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2v-7h-2v7zM14 3v2h3.59l-9.83 9.83 1.41 1.41L19 6.41V10h2V3h-7z" />
+                                        </svg>
+                                    </div>
+                                    Cancel Cause
+                                </button>
+                            }
                         </div>
                     }
                 </div>
-
             </div>
             }
         </>
-
     )
 }
