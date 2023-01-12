f7765dbd8ca46912ed9b82a57f209ebcf7b7f73c
= <hvpetrow@gmail.com>
2023-01-12 19:20:08 +0100

    footer style for mobile implemented

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,7 +3,9 @@ import styles from './Footer.module.css';
 export const Footer = () => {
     return (
         <footer className={styles['footer']}>
-            <p className={styles['footer-content']}>All rights reserved | Hristo Petrov | Copyright &copy; {new Date().getFullYear()}</p>
+
+            <p className={styles['footer-content']}>Copyright &copy; {new Date().getFullYear()} | Hristo Petrov</p>
+            <p className={styles['footer-content']}>All rights reserved</p>
             <a className={styles['footer-link']} href="https://softuni.bg/">Software University</a>
         </footer>
     );
