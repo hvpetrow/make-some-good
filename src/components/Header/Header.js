711f9a6d08b211116ce4fbc0efc5824a4189ce9b
= <hvpetrow@gmail.com>
2023-01-12 16:51:06 +0100

    responsive style updated

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -34,33 +34,86 @@ export const Header = () => {
         setIsOpen(false);
     }
 
-    const dropdownMenu = <div className={styles['dropdown']}>
+    const profileDropdownMenu = <div className={styles['dropdown']}>
         <Link to="/my-profile" onClick={closeDropdownHandler} >My Profile</Link>
         <Link to="/my-causes" onClick={closeDropdownHandler}>My Causes</Link>
         <Link to="/logout" onClick={closeDropdownHandler}>Sign out</Link>
     </div>;
+
+    const mainDropdownMenu = <div className={styles['dropdown']}>
+        <Link to="/" onClick={closeDropdownHandler} >Home</Link>
+        <Link to="/catalog" onClick={closeDropdownHandler}>Causes</Link>
+        <Link to="/search" onClick={closeDropdownHandler}>Search</Link>
+        <Link to="/donate" onClick={closeDropdownHandler}>Donate</Link>
+    </div>;
+
+    const mobileMenu = <div className={styles['mobile-menu']}>
+        {/* Mobile menu button*/}
+        <button
+            type="button"
+            className={styles['mobile-menu-btn']}
+            aria-controls="mobile-menu"
+            aria-expanded="false"
+        >
+            <svg
+                className="block h-6 w-6"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="currentColor"
+                aria-hidden="true"
+            >
+                <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+                />
+            </svg>
+            {/*
+        Icon when menu is open.
+        Menu open: "block", Menu closed: "hidden"
+      */}
+            <svg
+                className="hidden h-6 w-6"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="currentColor"
+                aria-hidden="true"
+            >
+                <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M6 18L18 6M6 6l12 12"
+                />
+            </svg>
+        </button>
+    </div>
 
     return (
         <header>
             <nav>
                 <div className="left-side">
                     <ul>
-                        <li><Link to="/">Home</Link></li>
-                        <li><Link to="/catalog">Causes</Link></li>
-                        <li><Link to="/search">Search</Link></li>
-                        <li><Link to="/donate">Donate</Link></li>
+                        <li className={styles['nav-link']}><Link to="/">Home</Link></li>
+                        <li className={styles['nav-link']}><Link to="/catalog">Causes</Link></li>
+                        <li className={styles['nav-link']}><Link to="/search">Search</Link></li>
+                        <li className={styles['nav-link']}><Link to="/donate">Donate</Link></li>
+                        {mobileMenu}
                     </ul>
                 </div>
                 <div className="right-side">
                     {!currentUser
                         ? <ul className='unlogged-ul'>
-                            <li><Link to="/login" aria-current="page">Log In</Link></li>
-                            <li><Link to="/register" aria-current="page">Sign Up</Link></li>
+                            <li className={styles['nav-link']}><Link to="/login" aria-current="page">Log In</Link></li>
+                            <li className={styles['nav-link']}><Link to="/register" aria-current="page">Sign Up</Link></li>
                         </ul>
                         : <div className="logged-in">
                             <ul className={styles['logged-ul']}>
-                                <li><Link to="/create-cause">Create Cause</Link></li>
-                                <li><Link to="/joinedCauses">Joined Causes</Link></li>
+                                <li className={styles['nav-link']}><Link to="/create-cause">Create Cause</Link></li>
+                                <li className={styles['nav-link']}><Link to="/joinedCauses">Joined Causes</Link></li>
                                 <div ref={menuRef} >
                                     <li className={styles['profile']}>
                                         <button
@@ -78,7 +131,7 @@ export const Header = () => {
                                             />
                                         </button>
                                     </li>
-                                    {isOpen && dropdownMenu}
+                                    {isOpen && profileDropdownMenu}
                                 </div>
                             </ul>
                         </div>
